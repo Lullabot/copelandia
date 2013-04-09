cf221265b5b05faf51d922e64da3d21d427ca284
Jerad Bitner <sirkitree@gmail.com>
2013-04-08 20:35:53 -0600

    removing Drupal factory and using $http.get

--- clients/angularjs/app/scripts/app.js
+++ clients/angularjs/app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('app', ['node', 'nodes'])
+angular.module('app', [])
   .config(function($routeProvider) {
     $routeProvider
       .when('/', {
--- clients/angularjs/app/scripts/controllers/main.js
+++ clients/angularjs/app/scripts/controllers/main.js
@@ -7,6 +7,7 @@ function RecipeCtrl($scope, $http) {
       // this callback will be called asynchronously
       // when the response is available
       console.log(data);
+      $scope.recipes = data;
     }).
     error(function(data, status, headers, config) {
       // called asynchronously if an error occurs
