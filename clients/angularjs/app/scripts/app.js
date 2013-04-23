b8de382d3a3cc33078917b8b2605aef5239a0045
Jerad Bitner <sirkitree@gmail.com>
2013-04-23 16:21:55 -0600

    Large changes to the $http call, using factories instead.
    Adding factories for Node, Nodes, and File.
    Rewriting controller to use the new factories.

--- clients/angularjs/app/scripts/app.js
+++ clients/angularjs/app/scripts/app.js
@@ -1,13 +1,32 @@
 'use strict';
 
-angular.module('app', [])
-  .config(function($routeProvider) {
-    $routeProvider
-      .when('/', {
+angular.module('app', ['ngResource']).
+
+  // Node.get(:nid);
+  factory('Node', function($resource) {
+    var Node = $resource('http://copelandia.lulladev.com/node/:nid', {}, {});
+    return Node;
+  }).
+  
+  // Nodes.get();
+  factory('Nodes', function($resource) {
+    var Nodes = $resource('http://copelandia.lulladev.com/node.json', {}, {});
+    return Nodes;
+  }).
+
+  // File.get(:fid);
+  factory('File', function($resource) {
+    var File = $resource('http://copelandia.lulladev.com/file/:fid', {}, {});
+    return File;
+  }).
+
+  config(function($routeProvider) {
+    $routeProvider.
+      when('/', {
         templateUrl: 'views/main.html',
-        controller: 'RecipeCtrl'
-      })
-      .otherwise({
+        controller: 'recipeController'
+      }).
+      otherwise({
         redirectTo: '/'
       });
   });
--- clients/angularjs/app/scripts/controllers/main.js
+++ clients/angularjs/app/scripts/controllers/main.js
@@ -1,38 +1,25 @@
 'use strict';
 
-function RecipeCtrl($scope, $http) {
-  $scope.images = [];
+function recipeController($scope, Node, Nodes, File) {
+  // Init local cache.
+  $scope.cache = {};
 
-  $http.get('http://copelandia.lulladev.com/node.json').
-    success(function(data, status, headers, config) {
-      // this callback will be called asynchronously
-      // when the response is available
-      console.log('data', data);
-      var recipes = $scope.recipes = data;
-
-      // This sucks, but we have to loop through the list and do an individual
-      // .get on the file path because of how restws returns the file info for
-      // file fields. @see https://github.com/Lullabot/copelandia/issues/10
-      for (var i = recipes.list.length - 1; i >= 0; i--) {
-        var photos = recipes.list[i].field_recipe_photos;
-
-        for (var x = photos.length - 1; x >= 0; x--) {
-          var path = photos[x].file.uri;
-
-          $http.get(path + '.json').
-            success(function(data, status, headers, config) {
-              $scope.images.push(data.fid = data.url);
-            }).
-            error(function(data, status, headers, config) {
-              // called asynchronously if an error occurs
-              // or server returns response with an error status.
-            });
+  var recipes = $scope.recipes = Nodes.get();
+  
+  $scope.getImages = function(recipes) {
+    for (var i = recipes.list.length - 1; i >= 0; i--) {
+      if (recipes.list[i].field_recipe_photos.length !== 0) {
+        $scope.cache['images'] = $scope.cache['images'] || {};
+        if ($scope.cache['images'][i]) {
+          // Get values from cache.
+          $scope.recipes.list[i].image = $scope.cache['images'][i];
+        } else {
+          // Call server.
+          $scope.recipes.list[i].image = File.get({fid: recipes.list[i].field_recipe_photos[0].file.id});
+          $scope.cache['images'][i] = $scope.recipes.list[i].image;
         }
-      };
+      } 
+    }
+  };
 
-    }).
-    error(function(data, status, headers, config) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-    });
 }
