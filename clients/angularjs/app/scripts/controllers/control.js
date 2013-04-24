2f6f3917b908f2a38f3a7b855e28fa12def28b89
Jerad Bitner <sirkitree@gmail.com>
2013-04-24 14:48:32 -0600

    simplifying logic for retrieving images.

--- clients/angularjs/app/scripts/controllers/control.js
+++ clients/angularjs/app/scripts/controllers/control.js
@@ -6,19 +6,18 @@ function recipeController($scope, Node, Nodes, File) {
 
   var recipes = $scope.recipes = Nodes.get();
   
-  $scope.getImages = function(recipes) {
-    for (var i = recipes.list.length - 1; i >= 0; i--) {
-      if (recipes.list[i].field_recipe_photos.length !== 0) {
-        $scope.cache['images'] = $scope.cache['images'] || {};
-        if ($scope.cache['images'][i]) {
-          // Get values from cache.
-          $scope.recipes.list[i].image = $scope.cache['images'][i];
-        } else {
-          // Call server.
-          $scope.recipes.list[i].image = File.get({fid: recipes.list[i].field_recipe_photos[0].file.id});
-          $scope.cache['images'][i] = $scope.recipes.list[i].image;
-        }
+  $scope.getImage = function(recipe) {
+    if (recipe.field_recipe_photos.length !== 0) {
+      var fileId = recipe.field_recipe_photos[0].file.id;
+      
+      $scope.cache['images'] = $scope.cache['images'] || {};
+      if (!$scope.cache['images'][fileId]) {
+        // Call server
+        $scope.cache['images'][fileId] = File.get({fid: fileId});
       } 
+      
+      // Get value from cache;
+      return $scope.cache['images'][fileId].url;
     }
   };
 
