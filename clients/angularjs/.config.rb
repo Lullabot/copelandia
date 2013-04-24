772a475fa644cc62edfebabc28e232d207a269a4
Micah Godbolt <micahgodbolt@gmail.com>
2013-04-23 18:53:18 -0700

    init protoangular commit

--- clients/angularjs/.config.rb
+++ clients/angularjs/.config.rb
@@ -1,13 +1,15 @@
-require 'zurb-foundation'
+
+require "sass-globbing"
+require "susy"
 # Require any additional compass plugins here.
 
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "app/css"
+sass_dir = "app/scss"
+images_dir = "app/images"
+javascripts_dir = "app/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
