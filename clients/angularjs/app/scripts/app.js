e8246bc126f60697c96abc2db251f9ab04a5abbe
Sean Lange <sean@webthingee.com>
2013-04-26 18:58:18 -0700

    update routes to add a recipe view

--- clients/angularjs/app/scripts/app.js
+++ clients/angularjs/app/scripts/app.js
@@ -1,16 +1,27 @@
 'use strict';
 
-var app = angular.module('app', ['resource.nodes', 'resource.node', 'resource.file', 'directive.protobot']).
+var app = angular.module('app', ['resource.node', 'resource.file', 'directive.protobot']).
 
   config(function($routeProvider) {
     $routeProvider.
       when('/', {
         templateUrl: 'views/main.html'
       }).
-      when('/prototest', {
-        templateUrl: 'views/prototest.html'
+      when('/recipes', {
+        templateUrl: 'views/recipes.html'
+      }).
+      when('/recipe/:id', {
+        templateUrl: 'views/partials/recipe_view.html',
+        controller: 'RecipeView'
       }).
       otherwise({
         redirectTo: '/'
       });
   });
+
+function RecipeView($scope, $routeParams, Node) {
+    $scope.ctrl = 'RecipeView';
+
+    var RecipeNode = $scope.recipe = Node.GetNode( {id: $routeParams.id} );
+    console.log(RecipeNode);
+}
