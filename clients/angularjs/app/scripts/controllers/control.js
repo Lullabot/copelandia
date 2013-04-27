c877438688d690949a0ff786b6ab20d125dfd83b
Micah Godbolt <micahgodbolt@gmail.com>
2013-04-27 14:02:53 -0700

    added dynamic routing route and controller

--- clients/angularjs/app/scripts/controllers/control.js
+++ clients/angularjs/app/scripts/controllers/control.js
@@ -22,3 +22,13 @@ function recipeController($scope, Node, File) {
   };
 
 }
+
+function PagesController($scope, $http, $route, $routeParams, $compile) {
+  $route.current.templateUrl = 'views/' + $routeParams.name + ".html";
+
+  $http.get($route.current.templateUrl).then(function (msg) {
+    $('.container').html($compile(msg.data)($scope));
+  });
+
+}
+PagesController.$inject = ['$scope', '$http', '$route', '$routeParams', '$compile'];
