72d181aa376880b8ba430545811d9cce2ae7352b
Brock Boland <brock@brockboland.com>
2013-05-05 14:58:56 -0600

    Added empty Xcode project

---
+++ clients/ios/Copelandia/COPAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  COPAppDelegate.h
+//  Copelandia
+//
+//  Created by Brock Boland on 5/5/13.
+//  Copyright (c) 2013 Lullabot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface COPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ clients/ios/Copelandia/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Copelandia
+//
+//  Created by Brock Boland on 5/5/13.
+//  Copyright (c) 2013 Lullabot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "COPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([COPAppDelegate class]));
+    }
+}
